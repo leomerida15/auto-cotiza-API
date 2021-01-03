d8691b9bcb8b355a8b5b30a06bcae43b61c5b5d5
leomerida15 <dimasmerida15@gmail.com>
2021-01-03 16:33:49 -0400

    sdfcs

--- src/app.js
+++ src/app.js
@@ -8,35 +8,22 @@ const app = express();
 const routes = require('./routers/index.routes');
 app.use(cors('*'));
 
-console.log(1);
-
 // db
 require('./db/');
-
-console.log(2);
 
 // Settings
 app.set('port', process.env.PORT || 5000);
 
-console.log(3);
-
-console.table([app.get('port')]);
-
 // Middlewares
 app.use(express.json());
 
-console.log(4);
-
 // Routes
 app.use('/uploads', express.static(path.resolve(__dirname, 'uploads')));
-// app.use(express.static(path.resolve(__dirname, 'public/dist')));
+app.use(express.static(path.resolve(__dirname, 'public/dist')));
 
 app.get('/', function (req, res) {
-	// res.sendFile(path.resolve(__dirname, 'public/dist') + '/index.html');
-	console.log(5);
+	res.sendFile(path.resolve(__dirname, 'public/dist') + '/index.html');
 });
-
-console.log(6);
 
 routes(app);
 
@@ -45,8 +32,6 @@ app.use(err_404);
 
 // init server
 app.listen(app.get('port'), () => {
-	// console.clear();
-
 	console.log('                                                                  ()_()');
 	console.log(`app corriendo en el puerto http://localhost:${app.get('port')} leoM             (o.o)`);
 	console.log('                                                                  (|_|)*');
